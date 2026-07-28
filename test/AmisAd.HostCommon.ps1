0c8b7f5c9b97f5246d48195ef24cad2998763a86
Alisson Sol <git@AlissonSol.com>
2026-07-27 22:33:09 -0400

    Version update

--- test/AmisAd.HostCommon.ps1
+++ test/AmisAd.HostCommon.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 2026.07.28
+.VERSION 2026.07.31
 .GUID 4283c1d7-2a94-4e60-b8d1-3c7e6a09f452
 .AUTHOR Alisson Sol et al.
 .Copyright (c) 2026 by Alisson Sol et al.
--- test/Set-Resource.ps1
+++ test/Set-Resource.ps1
@@ -1,5 +1,5 @@
 <#PSScriptInfo
-.VERSION 2026.07.28
+.VERSION 2026.07.31
 .GUID 42b4e8da-6f32-4c9e-ad57-8b1c3f74d2e6
 .AUTHOR Alisson Sol et al.
 .Copyright (c) 2026 by Alisson Sol et al.
